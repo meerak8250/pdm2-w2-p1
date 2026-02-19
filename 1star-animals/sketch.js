01c64fe90ede8a9450639976caced4b34de911cf
Meera Kofkin <meerakofkin05@gmail.com>
2026-02-19 13:25:42 +0000

    Update sketch.js

--- 1star-animals/sketch.js
+++ 1star-animals/sketch.js
@@ -1,13 +1,26 @@
-let animal1;
+//let animal1;
+let bear;
+let cat;
+let monkey;
+
+function preload() {
+    bear = loadImage("assets/bear.jpg")
+    cat = loadImage("assets/cat.jpg")
+    monkey = loadImage("assets/monkey.jpg")
+}
 
 function setup() {
-    // TODO create the canvas
-    animal1 = new Animal(); // TODO fill in the parameters
+    createCanvas(400, 400);
+    bear = new Animal(bear);
+    cat = new Animal(cat);
+    monkey = new Animal(monkey);
 }
 
 function draw() {
-    // TODO draw the background
-    //animal1.display();
+    background(255);
+    bear.display();
+    cat.display();
+    monkey.display();
 }
 
 function keyPressed() {
